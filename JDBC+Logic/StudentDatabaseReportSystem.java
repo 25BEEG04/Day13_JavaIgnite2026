c76b22d9a465a7c5fa54686a39a8cbe4e2cb987b
rite2harini <rite2harini@gmail.com>
2026-06-09 13:18:28 +0530

    Create StudentDatabaseReportSystem.java

---
+++ JDBC+Logic/StudentDatabaseReportSystem.java
@@ -0,0 +1,20 @@
+/*
+Create a Java program that connects to a MySQL database college_db.
+
+The table students contains:
+
+id
+name
+marks
+Task:
+Fetch all student records
+Display only students who scored above average marks
+Also print total number of students processed
+Hint:
+
+Think in steps:
+
+Fetch all data using ResultSet
+First calculate average marks
+Then filter while reading result
+*/
